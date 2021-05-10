d4ecd45a74844f73a6a21b1fdf2aa9f7f08605c2
boxabhi <abhijeetg40@gmail.com>
2021-05-10 15:14:44 +0530

    -

--- core/settings.py
+++ core/settings.py
@@ -135,5 +135,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ckabhijeet@gmail.com'
-EMAIL_HOST_PASSWORD = 'hello@citykonnect123'
